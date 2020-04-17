5231f8277bd0193ee959a28ebafee603cacfe1a6
Nikolaos Kokolantonakis <eukprice@gmail.com>
2020-04-17 22:30:38 +0300

    To implement gauss jordan

--- erg_2/bankAccount/bank.c
+++ erg_2/bankAccount/bank.c
@@ -2,33 +2,36 @@
 #include <limits.h>
 #include <math.h>
 
-int calculatePolynomial(int degree, int value, int password, int ai[])
+int calculatePolynomial(int degree, int point, int password, int ai[])
 {
     int coef;
     int calculation;
 
-    value = password;
+    point = password;
 
     for (coef = 0; coef < 4; coef++)
     {
-        calculation = ai[coef] *pow(degree,coef+1);
-        printf("----->%d\n",calculation);
-        value += calculation;
+        calculation = ai[coef] * pow(degree + 1, coef + 1);
+        point += calculation;
+        //printf("----->%d\n", point);
     }
-
-    return value;
+    printf("\n");
+    return point;
 }
 
 int main()
 {
 
     //ULONG_MAX
-    int values[10] = {};
+    int points[10] = {};
     int a0 = 23;
-    int ai[8] = {2, 2, 3, 4};
+    int ai[8] = {2, 2, 3, 4, 2, 2, 3, 3};
     int point;
+    int coef;
 
-    point = calculatePolynomial(5,point,a0,ai);
-
-    printf("%d\n",point);
+    for (coef = 0; coef < 5; coef++)
+    {
+        points[coef] = calculatePolynomial(coef, point, a0, ai);
+        printf("%d\n", points[coef]);
+    }
 }
