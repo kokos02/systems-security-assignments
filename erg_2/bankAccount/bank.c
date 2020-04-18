c14349f07425daeafd5758f24ce135c60ef6ca13
Nikolaos Kokolantonakis <eukprice@gmail.com>
2020-04-18 14:25:34 +0300

    +gauss elimination with console input matrix

--- erg_2/bankAccount/bank.c
+++ erg_2/bankAccount/bank.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <limits.h>
 #include <math.h>
+#include <stdlib.h>
+#define   SIZE   10
 
 int calculatePolynomial(int degree, int point, int password, int ai[])
 {
@@ -34,4 +36,60 @@ int main()
         points[coef] = calculatePolynomial(coef, point, a0, ai);
         printf("%d\n", points[coef]);
     }
+
+     float matrix[SIZE][SIZE], x[SIZE], ratio;
+	 int row,column,k,unknown;
+	
+	 
+	
+	 /* Inputs */
+	 /* 1. Reading number of unknowns */
+	 printf("Enter number of unknowns: ");
+	 scanf("%d", &unknown);
+	 /* 2. Reading Augmented Matrix */
+	 for(row=1;row<=unknown;row++)
+	 {
+		  for(column=1;column<=unknown+1;column++)
+		  {
+			   printf("matrix[%d][%d] = ",row,column);
+			   scanf("%f", &matrix[row][column]);
+		  }
+	 }
+	/* Applying Gauss Elimination */
+	 for(row=1;row<=unknown-1;row++)
+	 {
+		  if(matrix[row][row] == 0.0)
+		  {
+			   printf("Mathematical Error!");
+			   exit(0);
+		  }
+		  for(column=row+1;column<=unknown;column++)
+		  {
+			   ratio = matrix[column][row]/matrix[row][row];
+			   
+			   for(k=1;k<=unknown+1;k++)
+			   {
+			  		matrix[column][k] = matrix[column][k] - ratio*matrix[row][k];
+			   }
+		  }
+	 }
+	 /* Obtaining Solution by Back Subsitution */
+	 x[unknown] = matrix[unknown][unknown+1]/matrix[unknown][unknown];
+	
+	 for(row=unknown-1;row>=1;row--)
+	 {
+		  x[row] = matrix[row][unknown+1];
+		  for(column=row+1;column<=unknown;column++)
+		  {
+		  		x[row] = x[row] - matrix[row][column]*x[column];
+		  }
+		  x[row] = x[row]/matrix[row][row];
+	 }
+	 /* Displaying Solution */ 
+	 printf("\nSolution:\n");
+	 for(row=1;row<=unknown;row++)
+	 {
+	  	printf("x[%d] = %0.3f\n",row, x[row]);
+	 }
+
 }
